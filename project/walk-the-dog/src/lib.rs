a998e3a29fe4c3a47673d81c7c12499bd90f49f6
Flip451 <flipflap451@gmail.com>
2023-11-18 12:27:31 +0900

    feat: sierpinski

--- project/walk-the-dog/src/lib.rs
+++ project/walk-the-dog/src/lib.rs
@@ -1,6 +1,8 @@
+use std::ops::Add;
+use std::ops::Mul;
+
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,7 +12,6 @@ use wasm_bindgen::JsCast;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -18,7 +19,6 @@ pub fn main_js() -> Result<(), JsValue> {
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
 
     // windowオブジェクトの取得
     let window = web_sys::window().expect("no global `window` exists");
@@ -35,14 +35,90 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 
-    context.move_to(300., 0.);
-    context.begin_path();
-    context.line_to(0., 600.);
-    context.line_to(600., 600.);
-    context.line_to(300., 0.);
-    context.close_path();
-    context.stroke();
-    context.fill();
+    let triangle = Triangle {
+        p1: Point { x: 300.0, y: 0. },
+        p2: Point { x: 0., y: 600.0 },
+        p3: Point { x: 600.0, y: 600.0 },
+    };
+
+    // context.set_fill_style(&"rgb(150,50,0)".into());
+    draw_sierpinski(&context, &triangle, 6);
 
     Ok(())
+}
+
+#[derive(Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Mul<f64> for Point {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+struct Triangle {
+    p1: Point,
+    p2: Point,
+    p3: Point,
+}
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, triangle: &Triangle) {
+    context.move_to(triangle.p1.x, triangle.p1.y);
+    context.line_to(triangle.p2.x, triangle.p2.y);
+    context.line_to(triangle.p3.x, triangle.p3.y);
+    context.line_to(triangle.p1.x, triangle.p1.y);
+    context.close_path();
+    context.stroke();
+    // context.fill();
+}
+
+fn draw_sierpinski(context: &web_sys::CanvasRenderingContext2d, triangle: &Triangle, depth: usize) {
+    if depth <= 0 {
+        return;
+    }
+
+    draw_triangle(context, triangle);
+
+    let center_p1_p2 = (triangle.p1 + triangle.p2) * 0.5;
+    let center_p2_p3 = (triangle.p2 + triangle.p3) * 0.5;
+    let center_p3_p1 = (triangle.p3 + triangle.p1) * 0.5;
+
+    let new_triangle_1 = Triangle {
+        p1: triangle.p1,
+        p2: center_p1_p2,
+        p3: center_p3_p1,
+    };
+    let new_triangle_2 = Triangle {
+        p1: center_p1_p2,
+        p2: triangle.p2,
+        p3: center_p2_p3,
+    };
+    let new_triangle_3 = Triangle {
+        p1: center_p3_p1,
+        p2: center_p2_p3,
+        p3: triangle.p3,
+    };
+
+    draw_sierpinski(context, &new_triangle_1, depth - 1);
+    draw_sierpinski(context, &new_triangle_2, depth - 1);
+    draw_sierpinski(context, &new_triangle_3, depth - 1);
 }
