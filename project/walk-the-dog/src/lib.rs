a5922f2cefd5fcd2374ea7fa71e8d7a1933915e7
Flip451 <flipflap451@gmail.com>
2023-11-18 11:50:27 +0900

    feat: add feature to draw a triangle

--- project/walk-the-dog/src/lib.rs
+++ project/walk-the-dog/src/lib.rs
@@ -1,5 +1,5 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::JsCast;
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -20,8 +20,29 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    // windowオブジェクトの取得
+    let window = web_sys::window().expect("no global `window` exists");
+    // documentオブジェクトの取得
+    let document = window.document().expect("should have a document on window");
+    // canvas要素の取得 (Element型)
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    // Element 型の canvas を HtmlCanvasElement にキャスト
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    // コンテキストの取得
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+
+    context.move_to(300., 0.);
+    context.begin_path();
+    context.line_to(0., 600.);
+    context.line_to(600., 600.);
+    context.line_to(300., 0.);
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
 }
