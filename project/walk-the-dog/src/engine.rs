54f1eecd69dd131cf7a0a05416fe63b2d14eef8b
Flip451 <flipflap451@gmail.com>
2023-11-20 12:20:53 +0900

    feat: add timestep correction

--- project/walk-the-dog/src/engine.rs
+++ project/walk-the-dog/src/engine.rs
@@ -48,11 +48,24 @@ pub trait Game {
     fn draw(&self, context: &CanvasRenderingContext2d);
 }
 
-pub struct GameLoop;
+const FRAME_SIZE: f32 = 1. / 60. * 1000.;
+
+pub struct GameLoop {
+    // 直前のフレームがリクエストされた時刻
+    last_frame: f64,
+    // 最後に draw されてからの累積時間
+    accumulated_delta: f32,
+}
+
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
     pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+        let mut game_loop = GameLoop {
+            last_frame: browser::now()?,
+            accumulated_delta: 0.,
+        };
+
         // js における以下のコードを模したもの
         //   (なお requestAnimationFrameは渡した関数をブラウザの表示を邪魔しないタイミングで処理されるようにする関数)
         //   <https://rustwasm.github.io/docs/wasm-bindgen/examples/request-animation-frame.html> を参照せよ
@@ -69,7 +82,18 @@ impl GameLoop {
         let g = f.clone();
 
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
-            game.update();
+            // perf は、このコールバック関数が呼び出された時点の performance.now() の値（＝その時点の時刻）
+            // この値を用いて、前回のフレームからの経過時間を計算し、それを累積時間に加算する
+            game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
+
+            // 累積時間分だけ update を繰り返す
+            while game_loop.accumulated_delta > FRAME_SIZE {
+                game.update();
+                game_loop.accumulated_delta -= FRAME_SIZE;
+            }
+
+            game_loop.last_frame = perf;
+
             game.draw(&browser::context().expect("Getting canvas context failed"));
 
             browser::request_animation_frame(f.borrow().as_ref().unwrap()).unwrap();
